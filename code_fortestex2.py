39443d80f4b5622c00a5c550335cf7b1b4615411
UP-Polina-97 <87222067+UP-Polina-97@users.noreply.github.com>
2021-11-08 10:16:42 +0100

    Update code_fortestex2.py

--- code_fortestex2.py
+++ code_fortestex2.py
@@ -4,7 +4,7 @@ import json
 def yandex_disk_folder():
     url = "https://cloud-api.yandex.net:443/"
     url_extra_folder = "v1/disk/resources"
-    token = 'AQAAAABXPW06AAdM1-YiAlNVtE3xk5EvzmZuI7U'
+    token = ''
     headers = {
         'accept': 'application/json',
         'authorization': f'OAuth {token}'}
